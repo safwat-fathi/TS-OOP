3d778083e1a01416e66acc194fab005fa8428958
Safwat Fathi <safwat.fathi@naseej.com>
2024-10-03 13:37:08 +0300

    update: http client

--- src/SOLID/InterfaceSegregation.ts
+++ src/SOLID/InterfaceSegregation.ts
@@ -1,3 +1,48 @@
+interface IStorageService {
+	upload: (file: File) => Promise<void>;
+	delete: (filename: string) => Promise<void>;
+	getFileURL: (filename: string) => Promise<string>;
+	listFiles: () => Promise<string[]>;
+}
+
+interface IS3Service extends IStorageService {
+	setBucketPolicy: (policy: string) => Promise<void>;
+}
+
+interface IAzureBlobStorageService extends IStorageService {
+	setContainerACL: (acl: string) => Promise<void>;
+}
+
+interface IGoogleCloudStorageService extends IStorageService {
+	setMetadata: (filename: string, metadata: object) => Promise<void>;
+}
+
+interface ILocalStorageService extends IStorageService {
+	connectToLocalServer: (url: string) => Promise<void>;
+}
+
+class S3Service implements IS3Service {
+	async upload(file: File): Promise<void> {
+		console.log("uploading file to S3");
+	}
+
+	async delete(filename: string): Promise<void> {
+		console.log(`deleting file: ${filename} from S3`);
+	}
+
+	async getFileURL(filename: string): Promise<string> {
+		return `https://s3.amazonaws.com/${filename}`;
+	}
+
+	async listFiles(): Promise<string[]> {
+		return ["file1", "file2", "file3"];
+	}
+
+	async setBucketPolicy(policy: string): Promise<void> {
+		console.log("setting bucket policy: ", policy);
+	}
+}
+
 // * Before following ISP
 // interface Ports {
 //   useUSB: () => void;
@@ -43,53 +88,53 @@
 // }
 
 // * After following ISP
-interface CommonPorts {
-  useUSB: () => void;
-  useLAN: () => void;
-}
+// interface CommonPorts {
+//   useUSB: () => void;
+//   useLAN: () => void;
+// }
 
-interface ExtraPorts {
-  usePS2: () => void;
-  useVGA: () => void;
-}
+// interface ExtraPorts {
+//   usePS2: () => void;
+//   useVGA: () => void;
+// }
 
-interface DeluxePorts {
-  useUSB3: () => void;
-  useNFC: () => void;
-}
+// interface DeluxePorts {
+//   useUSB3: () => void;
+//   useNFC: () => void;
+// }
 
-export class PC implements CommonPorts, DeluxePorts, ExtraPorts {
-  useUSB() {
-    console.log("USB port is ready for your PC!");
-  }
+// export class PC implements CommonPorts, DeluxePorts, ExtraPorts {
+//   useUSB() {
+//     console.log("USB port is ready for your PC!");
+//   }
 
-  useLAN() {
-    console.log("LAN port is ready for your PC!");
-  }
+//   useLAN() {
+//     console.log("LAN port is ready for your PC!");
+//   }
 
-  usePS2() {
-    console.log("PS2 port is ready for your PC!");
-  }
+//   usePS2() {
+//     console.log("PS2 port is ready for your PC!");
+//   }
 
-  useVGA() {
-    console.log("VGA port is ready for your PC!");
-  }
+//   useVGA() {
+//     console.log("VGA port is ready for your PC!");
+//   }
 
-  useUSB3() {
-    console.log("USB3 port is ready for your PC!");
-  }
+//   useUSB3() {
+//     console.log("USB3 port is ready for your PC!");
+//   }
 
-  useNFC() {
-    console.log("NFC port is ready for your PC!");
-  }
-}
+//   useNFC() {
+//     console.log("NFC port is ready for your PC!");
+//   }
+// }
 
-export class Laptop implements CommonPorts {
-  useUSB() {
-    console.log("USB port is ready for your Laptop!");
-  }
+// export class Laptop implements CommonPorts {
+//   useUSB() {
+//     console.log("USB port is ready for your Laptop!");
+//   }
 
-  useLAN() {
-    console.log("LAN port is ready for your Laptop!");
-  }
-}
+//   useLAN() {
+//     console.log("LAN port is ready for your Laptop!");
+//   }
+// }
--- src/http-client.ts
+++ src/http-client.ts
@@ -1,91 +1,274 @@
-import axios, {
-  AxiosInstance,
-  AxiosProgressEvent,
-  AxiosRequestConfig,
-  AxiosResponse,
-  InternalAxiosRequestConfig,
-} from "axios";
+import queryString from "query-string";
 
+// ? implement abort signal
+// ? how to get or set auth token from/to
+
+class HttpError extends Error {
+	public statusCode: number;
+	public statusText: string;
+	public body: string;
+	public headers: Headers;
+
+	constructor(
+		statusCode: number,
+		statusText: string,
+		body: string,
+		headers: Headers
+	) {
+		super(`HTTP Error: ${statusCode} ${statusText}`);
+		Object.setPrototypeOf(this, HttpError.prototype); // Necessary when extending built-in classes
+		this.statusCode = statusCode;
+		this.statusText = statusText;
+		this.body = body;
+		this.headers = headers;
+	}
+}
+
+enum ResponseType {
+	JSON = "json",
+	TEXT = "text",
+	BLOB = "blob",
+	ARRAYBUFFER = "arraybuffer",
+}
+type TMethod = "GET" | "POST" | "PUT" | "DELETE";
+type RequestOptions = RequestInit & { signal?: AbortSignal } & Record<
+		string,
+		any
+	>;
+type RequestBody = string | FormData | ArrayBuffer;
+type RetryConfig = {
+	retries?: number;
+	delay?: number; // in milliseconds
+	backoff?: (retryCount: number) => number; // function to calculate delay
+	retryOn?: number[]; // HTTP status codes to retry on
+};
+abstract class HttpClientAbstract {
+	private _baseURL: string;
+
+	private _defaultRetryConfig: RetryConfig = {};
+
+	constructor(baseURL: string) {
+		this._baseURL = baseURL;
+		this._defaultRetryConfig = {
+			retries: 3,
+			retryOn: [408, 500, 502, 503, 504],
+			delay: 1000,
+			backoff: (retryCount: number) => {
+				const delay = 1000 * Math.pow(2, retryCount);
+				return delay;
+			},
+		};
+	}
+
+	private async _request<T>(
+		method: TMethod,
+		route: string,
+		params?: any,
+		options?: RequestOptions
+	): Promise<T> {
+		let url = this._baseURL + route;
+
+		if (params) {
+			const queryStringified = queryString.stringify(params, {
+				arrayFormat: "comma",
+			});
+
+			url += "?" + queryStringified;
+		}
+
+		const response = await fetch(url, {
+			method,
+			...options,
+		});
+
+		await this.handleError(response);
+
+		return this._parseResponse<T>(response, options?.responseType);
+	}
+
+	protected async get<T>(
+		route: string,
+		params?: any,
+		options?: RequestOptions
+	): Promise<T> {
+		return await this._request<T>("GET", route, params, options);
+	}
+
+	protected async post<T>(
+		route: string,
+		body: RequestBody,
+		params?: any,
+		options?: RequestOptions
+	): Promise<T> {
+		return await this._request<T>("POST", route, params, { ...options, body });
+	}
+
+	protected async put<T>(
+		route: string,
+		body: RequestBody,
+		params?: any,
+		options?: RequestOptions
+	): Promise<T> {
+		return await this._request<T>("PUT", route, params, { ...options, body });
+	}
+
+	protected async delete<T>(
+		route: string,
+		params?: any,
+		options?: RequestOptions
+	): Promise<T> {
+		return await this._request<T>("DELETE", route, params, options);
+	}
+
+	private _shouldRetry(error: any, retryOn: number[]): boolean {
+		if (error instanceof HttpError) {
+			return retryOn.includes(error.statusCode);
+		}
+		// For network errors or timeouts, you might have custom logic
+		return false;
+	}
+
+	private async _parseResponse<T>(
+		response: Response,
+		responseType: ResponseType
+	): Promise<T> {
+		switch (responseType) {
+			case ResponseType.JSON:
+				return response.json();
+			case ResponseType.TEXT:
+				return response.text() as unknown as T;
+			case ResponseType.BLOB:
+				return response.blob() as unknown as T;
+			case ResponseType.ARRAYBUFFER:
+				return response.arrayBuffer() as unknown as T;
+			default:
+				throw new Error(`Unsupported response type: ${responseType}`);
+		}
+	}
+
+	protected async handleError(response: Response): Promise<void> {
+		if (!response.ok) {
+			// Attempt to parse the error body
+			let errorBody: any;
+			try {
+				errorBody = await response.json();
+			} catch {
+				errorBody = await response.text();
+			}
+
+			// Throw a custom HttpError with detailed information
+			throw new HttpError(
+				response.status,
+				response.statusText,
+				errorBody,
+				response.headers
+			);
+		}
+	}
+	// ! not implemented yet
+	// 	private async _retry<T>( fn: () => Promise<T>, retryConfig: RetryConfig = this._defaultRetryConfig ): Promise<T> {
+	// 		try {
+	// 			let attempts = 0;
+
+	// 			const {retryOn,retries, delay, backoff} = retryConfig
+
+	// 			while (retries && attempts < retries) {
+	// 				try {
+	// 					return await fn();
+
+	// 				} catch (error) {
+	// 					attempts++;
+
+	// const shouldRetry = this._shouldRetry(error, retryOn);
+
+	// 					if (attempts > retries || !shouldRetry) {
+	// 						throw error;
+	// 					}
+
+	// 					let waitTime = delay;
+
+	// 					if (error instanceof HttpError) {
+	// 				}
+	// 			}
+	// 		} catch (error) {
+	// 			// if (retries > 0) {
+	// 			// 	return this._retry(fn, retries - 1);
+	// 			// } else {
+	// 			// 	throw error;
+	// 			// }
+
+	// 		}
+	// 	}
+
+	// protected abstract delete: <T>(route: string, params?: any, options?: RequestOptions) => Promise<T>;
+	// protected abstract upload: <T>(
+	// 	route: string,
+	// 	body: RequestBody,
+	// 	onUploadProgress?: any,
+	// 	options?: RequestOptions
+	// ) => Promise<T>;
+}
 class HttpClient {
-  private static _instance: HttpClient | null = null;
-  private readonly _axios_instance: AxiosInstance;
-  private readonly _auth_token: string;
+	private static _instance: HttpClient | null = null;
 
-  private constructor(baseURL: string) {
-    this._auth_token = localStorage.getItem("auth_token") || ""; // Get auth token from local storage or other source
+	private readonly _auth_token: string;
 
-    this._axios_instance = axios.create({
-      baseURL,
-      headers: {
-        Authorization: `Bearer ${this._auth_token}`, // Add auth token to request headers
-      },
-    });
+	private constructor(baseURL: string) {
+		this._auth_token = localStorage.getItem("auth_token") || ""; // Get auth token from local storage or other source
+	}
 
-    this._axios_instance.interceptors.request.use(
-      (config: InternalAxiosRequestConfig) => config,
-      (error: any) => Promise.reject(error)
-    );
+	public static get instance() {
+		if (this._instance === null)
+			this._instance = new HttpClient("XXXXXXXXXXXXXXXXXXXXX");
+		return this._instance;
+	}
 
-    this._axios_instance.interceptors.response.use(
-      (response: AxiosResponse) => {
-        return response;
-      },
-      (error: any) => Promise.reject(error)
-    );
-  }
+	// public async get<T>(url: string, config?: AxiosRequestConfig): Promise<T> {
+	// 	const response = await this._axios_instance.get<T>(url, config);
 
-  public static get instance() {
-    if (this._instance === null)
-      this._instance = new HttpClient("XXXXXXXXXXXXXXXXXXXXX");
-    return this._instance;
-  }
+	// 	return response.data;
+	// }
 
-  public async get<T>(url: string, config?: AxiosRequestConfig): Promise<T> {
-    const response = await this._axios_instance.get<T>(url, config);
+	// public async post<T>(
+	// 	url: string,
+	// 	data?: any,
+	// 	config?: AxiosRequestConfig
+	// ): Promise<T> {
+	// 	const response = await this._axios_instance.post<T>(url, data, config);
 
-    return response.data;
-  }
+	// 	return response.data;
+	// }
 
-  public async post<T>(
-    url: string,
-    data?: any,
-    config?: AxiosRequestConfig
-  ): Promise<T> {
-    const response = await this._axios_instance.post<T>(url, data, config);
+	// public async put<T>(
+	// 	url: string,
+	// 	data?: any,
+	// 	config?: AxiosRequestConfig
+	// ): Promise<T> {
+	// 	const response = await this._axios_instance.put<T>(url, data, config);
 
-    return response.data;
-  }
+	// 	return response.data;
+	// }
 
-  public async put<T>(
-    url: string,
-    data?: any,
-    config?: AxiosRequestConfig
-  ): Promise<T> {
-    const response = await this._axios_instance.put<T>(url, data, config);
+	// public async delete<T>(url: string, config?: AxiosRequestConfig): Promise<T> {
+	// 	const response = await this._axios_instance.delete<T>(url, config);
 
-    return response.data;
-  }
+	// 	return response.data;
+	// }
 
-  public async delete<T>(url: string, config?: AxiosRequestConfig): Promise<T> {
-    const response = await this._axios_instance.delete<T>(url, config);
+	// public async upload<T>(
+	// 	url: string,
+	// 	formData: FormData,
+	// 	onUploadProgress?: (progressEvent: AxiosProgressEvent) => void
+	// ): Promise<T> {
+	// 	const response = await this._axios_instance.post<T>(url, formData, {
+	// 		headers: {
+	// 			"Content-Type": "multipart/form-data",
+	// 		},
+	// 		onUploadProgress,
+	// 	});
 
-    return response.data;
-  }
-
-  public async upload<T>(
-    url: string,
-    formData: FormData,
-    onUploadProgress?: (progressEvent: AxiosProgressEvent) => void
-  ): Promise<T> {
-    const response = await this._axios_instance.post<T>(url, formData, {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-      onUploadProgress,
-    });
-
-    return response.data;
-  }
+	// 	return response.data;
+	// }
 }
 
 const httpClient = HttpClient.instance;
